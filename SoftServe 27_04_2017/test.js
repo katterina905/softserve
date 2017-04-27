545c65b532fc2d2bf7f908d342f0cd25bd411050
katterina905 <katterina905@gmail.com>
2017-04-27 16:20:22 +0300

    hw_27.04.2017

---
+++ SoftServe 27_04_2017/test.js
@@ -0,0 +1,9 @@
+QUnit.test( "creature-enemy", function( assert ) {
+    var result = new Enemy();
+    assert.ok(result, 'Passed!')
+});
+
+QUnit.test( "creature-kindom", function( assert ) {
+    var result = new Kindom();
+    assert.ok(result, 'Passed!')
+});
